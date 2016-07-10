9b209d31bd76c5ac1c95a308682baf609f0de5d3
rdalin82 <Robert.Dalin@gmail.com>
2016-07-10 11:57:09 -0500

    failing test with unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,21 @@
 from selenium import webdriver
-import time
+import unittest
 
-browser = webdriver.Chrome();
-browser.get("http://localhost:8000")
-time.sleep(5)
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
 
-browser.quit()
+  def setUp(self):
+    self.browser = webdriver.Chrome();
+    self.browser.implicitly_wait(3)
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def test_can_start_a_list_and_retrieve_it_later(self):
+    self.browser.get("http://localhost:8000")
+    time.sleep(5)
+    self.assertIn('To-Do', self.browser.title)
+    self.fail("Finish the test!")
+
+if __name__ == '__main__':
+  unittest.main()
+
